7d4cbe3b737742a7a0e04baf3ec23783096fb57b
Tommy Luong <70537020+tl2952@users.noreply.github.com>
2021-05-10 16:07:51 -0400

    Add files via upload

---
+++ project4/index.php
@@ -0,0 +1,12 @@
+<?php echo file_get_contents("html/header.html"); ?>
+<html>
+	<body>
+<h1>Welcome To POG Cafe!</h1>
+	<h2>Register Here</h2>
+<li><a href="registration.php">Register</a></li>
+
+</body>
+</html>
+
+
+
